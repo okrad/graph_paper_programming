0e2e0dbf5716c7a9a9186a561a7616ff94a7e7b3
okrad <enrico.triolo@gmail.com>
2021-12-11 14:50:22 +0100

    Add github repo link

--- src/lib/header/Header.svelte
+++ src/lib/header/Header.svelte
@@ -3,6 +3,11 @@
 
 <header>
 	<h1>Graph paper programming</h1>
+
+	<a href="https://github.com/okrad/graph_paper_programming" class="github-ribbon">
+		GitHub
+	</a>
+
 </header>
 
 <style>
@@ -10,6 +15,13 @@
 		font-family: Arial, Helvetica, sans-serif;
 		box-shadow: 0 1px 5px -3px rgba(0, 0, 0, .4);
 		background-color: #BCCCDC;
+		position: relative;
+		display: flex;
+	}
+
+	.github-ribbon {
+		line-height: 3;
+		margin-right: 1rem;
 	}
 
 	h1 {
@@ -17,5 +29,6 @@
 		padding: 1rem;
 		margin: -.5rem -.5rem -.5rem .5rem;
 		color: #334E68;
+		flex-grow: 1;
 	}
 </style>
